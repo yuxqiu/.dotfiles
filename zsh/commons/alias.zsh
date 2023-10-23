8f128ac94abc6b9fbc9f931b2b02442cbf8c9c51
Yuxiang Qiu <yuxqiu@proton.me>
2023-10-22 23:22:01 -0400

    update alias

--- zsh/commons/alias.zsh
+++ zsh/commons/alias.zsh
@@ -1,5 +1,11 @@
 # docker alias
 alias dockps="docker ps --format \"{{.ID}} {{.Names}}\""
+alias docklsc="docker ps -a"
+alias dockrmc="docker rm"
+alias docklsi="docker images -a"
+alias dockrmi="docker rmi"
+
+# docker compose
 alias dcbuild="docker-compose build"
 alias dcup="docker-compose up"
 alias dcdown="docker-compose down"
--- zsh/linux/alias.zsh
+++ zsh/linux/alias.zsh
@@ -1,2 +1,6 @@
-#list available jdks
+# docker
+alias dockup="sudo systemctl start docker"
+alias dockdown="sudo systemctl stop docker"
+
+# list available jdks
 alias jdks="alternatives --config java"
