0cf6c5f7fc843b68ef74dbbc23644470d67caf74
Yuxiang Qiu <yuxqiu@proton.me>
2023-10-26 09:01:24 -0400

    cleanup: remove vim

--- zsh/linux/alias.zsh
+++ zsh/linux/alias.zsh
@@ -1,6 +1,6 @@
 # docker
-alias dockup="sudo systemctl start docker"
-alias dockdown="sudo systemctl stop docker"
+alias dockup="systemctl start docker"
+alias dockdown="systemctl stop docker.socket"
 
 # list available jdks
 alias jdks="alternatives --config java"
