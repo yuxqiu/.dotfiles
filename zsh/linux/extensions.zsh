13c5d6a6693c8244fdbfce761dba084c3a1ad043
Yuxiang Qiu <yuxqiu@proton.me>
2023-10-26 18:00:12 -0400

    feat: update zsh

--- zsh/linux/extensions.zsh
+++ zsh/linux/extensions.zsh
@@ -1,2 +1,7 @@
 # fzf
 [ -f /usr/share/fzf/shell/key-bindings.zsh ] && source /usr/share/fzf/shell/key-bindings.zsh
+
+source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+source "$DOTFILES/zsh/linux/pyenv.zsh"
--- zsh/linux/linux.zsh
+++ zsh/linux/linux.zsh
@@ -1,4 +1,4 @@
 source "$DOTFILES/zsh/linux/alias.zsh"
 source "$DOTFILES/zsh/linux/env.zsh"
+
 source "$DOTFILES/zsh/linux/extensions.zsh"
-source "$DOTFILES/zsh/linux/pyenv.zsh"
