c4e4f0cb2ec1d7f76e305b3e61fbae509b66432d
yuxqiu <yuxqiu@proton.me>
2024-09-14 11:59:51 +0800

    feat: add go path to linux env

--- zsh/linux/env.zsh
+++ zsh/linux/env.zsh
@@ -2,4 +2,7 @@
 export PATH="$HOME/.cargo/bin:$PATH"
 
 # diff-highlight
-export PATH="/usr/share/git-core/contrib/:$PATH"
+export PATH="/usr/share/git-core/contrib:$PATH"
+
+# go
+export PATH="$(go env GOPATH)/bin:$PATH"
