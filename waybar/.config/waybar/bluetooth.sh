b2cb4127d63f17f5d0a7f09133fbcfaef91b28c1
yuxqiu <yuxqiu@proton.me>
2024-09-19 11:00:10 +0800

    waybar: update style

--- waybar/.config/waybar/bluetooth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if rfkill -o TYPE,SOFT | grep bluetooth | grep unblocked > /dev/null; then
-    # Bluetooth is enabled
-    rfkill block bluetooth
-else
-    rfkill unblock bluetooth
-fi
