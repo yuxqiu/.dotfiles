de7003ba0f8e5656a9459c908035d36c27d4cc48
yuxqiu <yuxqiu@proton.me>
2024-10-31 11:37:48 +0000

    feat: update dnf

--- config/dnf/dnf-export.sh
+++ config/dnf/dnf-export.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf repoquery --qf '%{name}' --userinstalled \
+dnf repoquery --qf '%{name}\n' --userinstalled \
  | grep -v -- '-debuginfo$' \
  | grep -v '\(^kernel\)\|\(asahi\)' > dnf.txt
