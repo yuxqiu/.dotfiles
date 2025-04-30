2859b3be0f61a25fbb4711e47c8426f13ef56ae6
yuxqiu <yuxqiu@proton.me>
2025-04-30 23:33:42 +0100

    feat: update firefox conf

--- firefox/user-overide.js
+++ firefox/user-overide.js
@@ -220,7 +220,6 @@ user_pref("default-browser-agent.enabled", false);
 // PREF: enable helpful features:
 user_pref("browser.urlbar.suggest.calculator", true);
 user_pref("browser.urlbar.unitConversion.enabled", true);
-user_pref("sidebar.expandOnHover", true);
 
 // PREF: PDF sidebar on load [HIDDEN]
 // 2=table of contents (if not available, will default to 1)
