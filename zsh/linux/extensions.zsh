fe6eae0f5e8d1011e27c50b3bedd3bb4def3abc0
yuxqiu <yuxqiu@proton.me>
2025-07-21 11:13:31 +0800

    feat: update zsh conf

--- zsh/linux/extensions.zsh
+++ zsh/linux/extensions.zsh
@@ -4,7 +4,4 @@
 source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
-# use zsh-autosuggest async mode
-ZSH_AUTOSUGGEST_USE_ASYNC=1
-
 source "$DOTFILES/zsh/linux/pyenv.zsh"
