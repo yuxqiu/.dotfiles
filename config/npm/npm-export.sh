8694ce6fc6e2c4a7ad86e57997f2a4a077c2eb2f
yuxqiu <yuxqiu@proton.me>
2025-09-08 21:12:45 +0100

    chore: remove unused config

--- config/npm/npm-export.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# https://stackoverflow.com/a/41199625
-npm list --global --parseable --depth=0 | sed '1d' | awk '{gsub(/\/.*\//,"",$1); print}' > npmlist.txt
--- config/npm/npm-import.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# https://stackoverflow.com/a/41199625
-xargs npm install --global < npmlist.txt
