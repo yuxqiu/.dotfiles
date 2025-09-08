b62de150516e126e6ca295da57a363f3698e01fa
yuxqiu <yuxqiu@proton.me>
2025-09-08 23:00:09 +0100

    feat: add nix deps

--- nix/modules/common/default.nix
+++ nix/modules/common/default.nix
@@ -1,4 +1,24 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ nixfmt nixd ];
+  home.packages = with pkgs; [
+    nixfmt
+    nixd
+
+    # alacritty
+    difftastic
+    # firefox
+    # fzf
+    # git
+    go
+    hyperfine
+    lazydocker
+    # neovim
+    pandoc
+    # ruby
+    tectonic
+    tokei
+    tree
+    typst
+    typos
+  ];
   home.stateVersion = "25.05";
 }
