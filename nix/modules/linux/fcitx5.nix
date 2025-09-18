49c40f982729083de2b531ff599e19afae3415ff
yuxqiu <yuxqiu@proton.me>
2025-09-18 18:54:11 +0800

    feat: update gtk conf

--- nix/modules/linux/fcitx5.nix
+++ nix/modules/linux/fcitx5.nix
@@ -61,4 +61,14 @@
       };
     };
   };
+
+  gtk = {
+    gtk2.extraConfig = ''
+      gtk-im-module = "fcitx"
+    '';
+
+    gtk3.extraConfig = { gtk-im-module = "fcitx"; };
+
+    gtk4.extraConfig = { gtk-im-module = "fcitx"; };
+  };
 }
--- nix/modules/linux/gtk.nix
+++ nix/modules/linux/gtk.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 
 {
-  # GTK configuration
-  # - dconf.service needs to be manually installed and enabled
   gtk = {
     enable = true; # Enables management of GTK configs
 
@@ -12,31 +10,19 @@
       package = pkgs.gnome-themes-extra; # Provides Adwaita icons/theme
     };
     iconTheme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-    font = {
-      name = "Noto Sans";
-      size = 10;
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
     };
     cursorTheme = {
-      name = "Adwaita-dark";
+      name = "Adwaita";
       size = 24;
-      package = pkgs.gnome-themes-extra;
+      package = pkgs.adwaita-icon-theme;
     };
 
     # GTK3 extra settings (adds to ~/.config/gtk-3.0/settings.ini)
     gtk3.extraConfig = {
-      gtk-toolbar-style = "GTK_TOOLBAR_ICONS";
-      gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
-      gtk-button-images = false;
-      gtk-menu-images = false;
       gtk-enable-event-sounds = false;
       gtk-enable-input-feedback-sounds = false;
-      gtk-xft-antialias = 1;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintfull";
-      gtk-xft-rgba = "rgb";
       gtk-application-prefer-dark-theme = true;
     };
 
@@ -44,16 +30,13 @@
     gtk4.extraConfig = {
       gtk-enable-event-sounds = false;
       gtk-enable-input-feedback-sounds = false;
-      gtk-xft-antialias = 1;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintfull";
-      gtk-xft-rgba = "rgb";
       gtk-application-prefer-dark-theme = true;
     };
   };
 
-  # skip gsettings/dconf integration
-  dconf.enable = false;
+  # gsettings/dconf integration
+  # - dconf is used by gtk module by default, so no need to conf it manually
+  # dconf.enable = true;
 
   home.sessionVariables = {
     # Force dark theme for GTK apps
