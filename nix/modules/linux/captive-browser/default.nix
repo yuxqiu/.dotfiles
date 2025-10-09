398757b9c209a9c764bd2397ae010f87935a90d3
yuxqiu <yuxqiu@proton.me>
2025-10-09 22:32:19 +0800

    fix: captive browser

--- nix/modules/linux/captive-browser/default.nix
+++ nix/modules/linux/captive-browser/default.nix
@@ -3,4 +3,7 @@
 {
   home.packages = with pkgs; [ captive-browser ];
   home.file.".config/captive-browser.toml".source = ./captive-browser.toml;
+
+  # Add a browser profile to firefox
+  programs.firefox.profiles."captive-browser" = { id = 1; };
 }
