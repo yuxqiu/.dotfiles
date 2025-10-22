3931a701874f06e73657ce34c5e0996ca6e75719
yuxqiu <yuxqiu@proton.me>
2025-10-23 02:14:46 +0800

    fix: systemd conf

--- nix/modules/linux/journald/journald.nix
+++ nix/modules/linux/journald/journald.nix
@@ -1,7 +1,15 @@
 { ... }: {
   config = {
     environment = {
-      etc = { "systemd/journald.conf.d".source = ./journald.conf.d; };
+      etc = {
+        # With mode, system-manager will copy the file to the destination.
+        # It's important to specify file here as using folder still leads
+        # to a symlink.
+        "systemd/journald.conf.d/usage.conf" = {
+          source = ./journald.conf.d/usage.conf;
+          mode = "0644";
+        };
+      };
     };
   };
 }
--- nix/modules/linux/logind/logind.nix
+++ nix/modules/linux/logind/logind.nix
@@ -1,7 +1,15 @@
 { ... }: {
   config = {
     environment = {
-      etc = { "systemd/logind.conf.d".source = ./logind.conf.d; };
+      etc = {
+        # With mode, system-manager will copy the file to the destination.
+        # It's important to specify file here as using folder still leads
+        # to a symlink.
+        "systemd/logind.conf.d/power.conf" = {
+          source = ./logind.conf.d/power.conf;
+          mode = "0644";
+        };
+      };
     };
   };
 }
