d263d315ea3044014b46a790ca68e5e0b9335e4b
yuxqiu <yuxqiu@proton.me>
2025-10-26 01:45:12 +0800

    feat: add dnscrypt

--- nix/modules/linux/dnscrypt-proxy/dnscrypt-proxy.nix
+++ nix/modules/linux/dnscrypt-proxy/dnscrypt-proxy.nix
@@ -1,5 +1,17 @@
 { ... }: {
   config = {
-    environment = { etc = { "dnscrypt-proxy".source = ./dnscrypt-proxy; }; };
+    environment = {
+      etc = {
+        "dnscrypt-proxy".source = ./dnscrypt-proxy;
+        "resolv.conf" = {
+          text = ''
+            nameserver ::1
+            nameserver 127.0.0.1
+            options edns0
+          '';
+          mode = "0644";
+        };
+      };
+    };
   };
 }
