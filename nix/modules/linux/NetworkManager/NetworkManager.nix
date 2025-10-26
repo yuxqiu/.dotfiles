5544640b0c7ffb5479dcfeec3559b15461ccde19
yuxqiu <yuxqiu@proton.me>
2025-10-27 01:32:56 +0800

    fix: NetworkManager

--- nix/modules/linux/NetworkManager/NetworkManager.nix
+++ nix/modules/linux/NetworkManager/NetworkManager.nix
@@ -1,7 +1,24 @@
 { ... }: {
   config = {
     environment = {
-      etc = { "NetworkManager/conf.d".source = ./conf.d; };
+      etc = {
+        "NetworkManager/conf.d/connectivity.conf" = {
+          source = ./conf.d/connectivity.conf;
+          mode = "0644";
+        };
+        "NetworkManager/conf.d/dns-servers.conf" = {
+          source = ./conf.d/dns-servers.conf;
+          mode = "0644";
+        };
+        "NetworkManager/conf.d/hostname.conf" = {
+          source = ./conf.d/hostname.conf;
+          mode = "0644";
+        };
+        "NetworkManager/conf.d/wifi_rand_mac.conf" = {
+          source = ./conf.d/wifi_rand_mac.conf;
+          mode = "0644";
+        };
+      };
     };
   };
 }
