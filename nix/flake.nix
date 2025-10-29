d6a86cea7aa3a79f64d324cd1780d6d03e26ab01
yuxqiu <yuxqiu@proton.me>
2025-10-29 10:45:24 +0800

    feat: update niri

--- nix/flake.nix
+++ nix/flake.nix
@@ -25,7 +25,7 @@
       # cache mirror located in China
 
       # status: https://mirror.sjtu.edu.cn/
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      # "https://mirror.sjtu.edu.cn/nix-channels/store"
 
       # status: https://mirrors.ustc.edu.cn/status/
       # "https://mirrors.ustc.edu.cn/nix-channels/store"
