be3b875100b750a42a836613499f73d196ef829c
yuxqiu <yuxqiu@proton.me>
2025-11-02 01:18:48 +0800

    feat: update nix

--- nix/modules/linux/zsh.nix
+++ nix/modules/linux/zsh.nix
@@ -15,23 +15,5 @@
       system-manager-update =
         "nix flake update && sudo $(which nix) run 'github:numtide/system-manager' -- switch --flake .#${system} && rm result";
     };
-
-    siteFunctions = {
-      proxy = ''
-        socks_proxy="socks5://127.0.0.1:1080"
-
-        if [[ -z "$1" ]]; then
-            echo "Usage: proxy <command> [args...]"
-            return 1
-        fi
-
-        env \
-            all_proxy="$socks_proxy" \
-            ALL_PROXY="$socks_proxy" \
-            socks_proxy="$socks_proxy" \
-            SOCKS_PROXY="$socks_proxy" \
-            zsh -ic "$*"
-      '';
-    };
   };
 }
