a18476665e1a9ae75d52e5bc5ba74e64aa20221c
yuxqiu <yuxqiu@proton.me>
2025-11-10 11:03:44 +0800

    feat: update nix

--- nix/modules/common/nixgl.nix
+++ nix/modules/common/nixgl.nix
@@ -1,7 +1,7 @@
 { nixGL, ... }:
 
 {
-  nixGL = {
+  targets.genericLinux.nixGL = {
     # Set packages to nixGL.packages for non-NixOS, null for NixOS
     packages = if builtins.pathExists /etc/NIXOS then null else nixGL.packages;
     vulkan.enable = true; # Enable Vulkan
