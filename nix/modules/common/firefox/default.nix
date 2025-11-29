1376b19458d566b2b80b820d1c8b69fc42077450
yuxqiu <yuxqiu@proton.me>
2025-11-30 01:24:10 +0800

    feat: update firefox

--- nix/modules/common/firefox/default.nix
+++ nix/modules/common/firefox/default.nix
@@ -120,8 +120,8 @@ in {
         "duckduckgo@search.mozilla.org" = { installation_mode = "blocked"; };
       };
     };
-    # Wait for https://github.com/NixOS/nixpkgs/pull/462094 to update flake
-    package = (config.lib.nixGL.wrap pkgs.firefox);
+    # Wait for https://github.com/NixOS/nixpkgs/pull/462094 to update
+    package = (config.lib.nixGL.wrap pkgs.firefox-bin);
   };
 
   home.file = {
