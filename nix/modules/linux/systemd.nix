3e4863cfe3afd848b02ba6926212fca4ff0e8b3c
yuxqiu <yuxqiu@proton.me>
2025-12-06 17:37:07 +0800

    feat: use systemd module

--- nix/modules/linux/systemd.nix
+++ nix/modules/linux/systemd.nix
@@ -1,8 +1,10 @@
-{ ... }: {
+{ config, ... }: {
   # allow systemd to discover user service files installed by nix
   # - https://unix.stackexchange.com/a/696035
-  xdg.configFile."systemd/user.conf".text = ''
-    [Manager]
-    ManagerEnvironment="XDG_DATA_DIRS=/home/yuxqiu/.nix-profile/share:/nix/var/nix/profiles/default/share"
-  '';
+  systemd.user.settings.Manager = {
+    ManagerEnvironment = {
+      XDG_DATA_DIRS =
+        "${config.home.profileDirectory}/share:/nix/var/nix/profiles/default/share";
+    };
+  };
 }
