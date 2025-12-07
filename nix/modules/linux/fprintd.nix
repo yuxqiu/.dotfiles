bb9b51076929955a5f35041e12ad05796a27720d
yuxqiu <yuxqiu@proton.me>
2025-12-07 23:37:08 +0800

    chore: write fprint script to nix store

--- nix/modules/linux/fprintd.nix
+++ nix/modules/linux/fprintd.nix
@@ -1,4 +1,22 @@
-{ pkgs, username, ... }: {
+{ pkgs, username, ... }:
+
+let
+  watchFprintd = pkgs.writeShellScriptBin "watch-fprintd.sh" ''
+    restart_fprintd() {
+        echo "[fprintd-watchdog] Restarting fprintd due to segfault or device reinsertion..."
+        systemctl kill fprintd || true
+        sleep 1 # sleeping is always healthy
+        systemctl restart fprintd
+    }
+
+    # Follow kernel messages
+    journalctl -kf -t kernel --cursor-file="/var/tmp/fprintd-journal.cursor" | while read -r line; do
+        if echo "$line" | grep -Eq "fprintd.*segfault|New USB.*found.*idVendor=2541.*idProduct=0236"; then
+            restart_fprintd
+        fi
+    done
+  '';
+in {
   config = {
     environment = {
       etc = {
@@ -17,32 +35,6 @@
     # https://github.com/ddlsmurf/libfprint-CS9711/issues/4
     # - This fork may segfault when unlocking screens, which will make unlock with fprint unusable
     # - The temporary solution below can minimize the impact of the segfault.
-    system-manager.preActivationAssertions.fprintd-watchdog = {
-      enable = true;
-      script = ''
-        install -d -m 755 /usr/local/bin
-        install -m 755 ${
-          pkgs.writeScript "watch-fprintd.sh" ''
-            #!/usr/bin/bash
-
-            restart_fprintd() {
-                echo "[fprintd-watchdog] Restarting fprintd due to segfaulti or device reinsertion..."
-                systemctl kill fprintd || true
-                sleep 1 # sleeping is always healthy
-                systemctl restart fprintd
-            }
-
-            # Follow kernel messages
-            journalctl -kf -t kernel --cursor-file="/var/tmp/fprintd-journal.cursor" | while read -r line; do
-                if echo "$line" | grep -Eq "fprintd.*segfault|New USB.*found.*idVendor=2541.*idProduct=0236"; then
-                    restart_fprintd
-                fi
-            done
-          ''
-        } /usr/local/bin/watch-fprintd.sh
-      '';
-    };
-
     systemd.services.fprintd-watchdog = {
       description =
         "Watch for fprintd segfaults or Goodix 2541:0236 re-plug and restart it";
@@ -51,8 +43,9 @@
       wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
-        Environment = "PATH=/run/system-manager/sw/bin/:/home/${username}/.local/bin:/home/${username}/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/usr/local/bin:/usr/bin";
-        ExecStart = "/usr/local/bin/watch-fprintd.sh";
+        Environment =
+          "PATH=/run/system-manager/sw/bin/:/home/${username}/.local/bin:/home/${username}/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/usr/local/bin:/usr/bin";
+        ExecStart = "${watchFprintd}/bin/watch-fprintd.sh";
         Restart = "always";
         RestartSec = 10;
         StandardOutput = "journal";
