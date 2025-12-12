fb3e19ae4c4314d419bc4f34f922a8a3802aa1c3
yuxqiu <yuxqiu@proton.me>
2025-12-12 10:36:42 +0800

    feat: add ssh default config

--- nix/modules/hm/common/ssh.nix
+++ nix/modules/hm/common/ssh.nix
@@ -2,6 +2,20 @@
   programs.ssh = {
     enable = true;
     package = pkgs.openssh;
+
+    # base config
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "no";
+      compression = false;
+      serverAliveInterval = 300;
+      serverAliveCountMax = 3;
+      hashKnownHosts = true;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+    };
+
+    includes = [ "config.d/*" ];
+    enableDefaultConfig = false;
   };
 
   services.ssh-agent = {
