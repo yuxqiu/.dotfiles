b2e1ffc81eb7efc3ebb0d19a7111c5eef1633b68
yuxqiu <yuxqiu@proton.me>
2025-12-18 09:49:27 +0800

    feat: update nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,9 +9,8 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # https://github.com/nix-community/nix-vscode-extensions/pull/161
     nix-vscode-extensions = {
-      url = "github:dseum/nix-vscode-extensions";
+      url = "github:nix-community/nix-vscode-extensions";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     proton-pass-cli = {
