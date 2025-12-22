542fb6f872a37e5b0b95d7ee1b3e50ec6a7cb03e
yuxqiu <yuxqiu@proton.me>
2025-12-22 12:50:23 +0800

    feat: cleanup gtk conf

--- nix/modules/hm/linux/gtk.nix
+++ nix/modules/hm/linux/gtk.nix
@@ -11,26 +11,5 @@
       package = pkgs.adwaita-icon-theme;
     };
     colorScheme = "dark";
-
-    # GTK3 extra settings (adds to ~/.config/gtk-3.0/settings.ini)
-    gtk3.extraConfig = {
-      gtk-enable-event-sounds = false;
-      gtk-enable-input-feedback-sounds = false;
-    };
-
-    # GTK4 extra settings (adds to ~/.config/gtk-4.0/settings.ini)
-    gtk4.extraConfig = {
-      gtk-enable-event-sounds = false;
-      gtk-enable-input-feedback-sounds = false;
-    };
-  };
-
-  # gsettings/dconf integration
-  # - dconf is used by gtk module by default, so no need to conf it manually
-  # dconf.enable = true;
-
-  home.sessionVariables = {
-    # Force dark theme for GTK apps
-    GTK_THEME = "Adwaita:dark";
   };
 }
