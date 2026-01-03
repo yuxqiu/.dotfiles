602f57420806c643195ee5ff578374346eb7e1dd
yuxqiu <yuxqiu@proton.me>
2026-01-03 22:23:09 +0800

    fix: opensnitch-ui crash

--- nix/hm/linux/opensnitch.nix
+++ nix/hm/linux/opensnitch.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [ opensnitch-ui ];
+  home.packages = [ pkgs.opensnitch-ui ];
+
+  # Add restart as opensnitch-ui is started too early and crashes consistently.
   services.opensnitch-ui.enable = true;
+  systemd.user.services.opensnitch-ui = {
+    Service = {
+      Restart = "on-failure";
+      RestartSec = "5";
+    };
+  };
 }
