aa86fa0ae888a3d83a37993b27bf93ba7bb0072c
yuxqiu <yuxqiu@proton.me>
2026-01-09 11:50:14 +0800

    feat: pass config to opensnitch directly

--- nix/sm/linux/opensnitch/default.nix
+++ nix/sm/linux/opensnitch/default.nix
@@ -1,26 +1,19 @@
 { pkgs, ... }:
 {
-  config = {
-    environment.etc = {
-      "opensnitchd/default-config.json".source = ./res/default-config.json;
-      "opensnitchd/system-fw.json".source = ./res/system-fw.json;
-    };
+  systemd.services.opensnitchd = {
+    description = "Application firewall OpenSnitch";
+    documentation = [ "https://github.com/evilsocket/opensnitch/wiki" ];
 
-    systemd.services.opensnitchd = {
-      description = "Application firewall OpenSnitch";
-      documentation = [ "https://github.com/evilsocket/opensnitch/wiki" ];
+    wantedBy = [ "multi-user.target" ];
 
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig = {
-        Type = "simple";
-        PermissionsStartOnly = true;
-        ExecStartPre = "/bin/mkdir -p /etc/opensnitchd/rules";
-        ExecStart = "${pkgs.opensnitch}/bin/opensnitchd -rules-path /etc/opensnitchd/rules";
-        Restart = "always";
-        RestartSec = 30;
-        TimeoutStopSec = 10;
-      };
+    serviceConfig = {
+      Type = "simple";
+      PermissionsStartOnly = true;
+      ExecStartPre = "/bin/mkdir -p /etc/opensnitchd/rules";
+      ExecStart = "${pkgs.opensnitch}/bin/opensnitchd -config-file ${./res/default-config.json} -fw-config-file ${./res/system-fw.json} -rules-path /etc/opensnitchd/rules";
+      Restart = "always";
+      RestartSec = 30;
+      TimeoutStopSec = 10;
     };
   };
 }
