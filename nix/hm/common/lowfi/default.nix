2eb14373f439538721930ea9bd28f4316a1e4da9
yuxqiu <yuxqiu@proton.me>
2026-01-14 14:49:10 +0800

    feat: add lofigirl track list

--- nix/hm/common/lowfi/default.nix
+++ nix/hm/common/lowfi/default.nix
@@ -1,8 +1,28 @@
-{ pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   lowfi = pkgs.callPackage ./lowfi.nix { };
+  trackList = ./lofigirl.txt;
+
+  # note: must symlink the txt as the created dbus name is directly related
+  # to the path of the provided track list
+  lofiWrapped = pkgs.writeShellScriptBin "lofigirl" ''
+    exec ${lib.getExe lowfi} --track-list "${config.xdg.configHome}/lofi/lofigirl.txt" "$@"
+  '';
+
 in
 {
-  home.packages = [ lowfi ];
+  xdg.configFile."lofi/lofigirl.txt" = {
+    source = trackList;
+  };
+
+  home.packages = [
+    lowfi
+    lofiWrapped
+  ];
 }
