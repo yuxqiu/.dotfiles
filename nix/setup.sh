dc284f53feb7e63b491a7036cee9e78568fd9351
yuxqiu <yuxqiu@proton.me>
2026-01-30 19:56:51 +0800

    feat: update nix setup

--- nix/setup.sh
+++ nix/setup.sh
@@ -6,15 +6,9 @@ set -e
 # Install nix from determinate
 curl -fsSL https://install.determinate.systems/nix | sh -s -- install
 
-# Install home manager
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-nix-env -iA home-manager.home-manager
-
-# Setup nix packages and configs
+# Setup home-manager packages and configs
 sys_name=$(uname -m)-$(uname -s | tr '[:upper:]' '[:lower:]')
-home-manager switch --flake .#$sys_name
-sudo ~/.nix-profile/bin/system-manager switch --flake .#$sys_name
+nix run home-manager/master -- switch --flake .#$sys_name
 
-# Cleanup nix
-home-manager expire-generations now && nix-collect-garbage
+# Setup system packages and configs
+sudo ~/.nix-profile/bin/system-manager switch --flake .#$sys_name
