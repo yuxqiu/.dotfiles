42a3def62eaac0e225a7eaa86cba6954eba31604
yuxqiu <yuxqiu@proton.me>
2026-03-02 01:44:34 +0800

    feat: nix: use flake-part generic attr

--- nix/profiles/hosts/yuxqiu-laptop.nix
+++ nix/profiles/hosts/yuxqiu-laptop.nix
@@ -9,13 +9,13 @@
       system = "aarch64-linux";
       stateVersion = "26.05";
       modules = [
+        config.flake.modules.generic.base
         config.flake.modules.homeManager.base
         config.flake.modules.homeManager.desktop
         config.flake.modules.homeManager.linux-base
         config.flake.modules.homeManager.linux-desktop
 
         config.flake.modules.homeManager.yuxqiu
-        config.flake.modules.homeManager.yuxqiu-laptop
       ];
     };
   };
@@ -24,6 +24,7 @@
     "yuxqiu-laptop" = {
       system = "aarch64-linux";
       modules = [
+        config.flake.modules.generic.base
         config.flake.modules.systemManager.base
         config.flake.modules.systemManager.desktop
 
@@ -33,15 +34,14 @@
     };
   };
 
-  flake.modules.homeManager.yuxqiu-laptop = {
+  flake.modules.generic.base = {
     my.sops.enable = true;
   };
 
   flake.modules.systemManager.yuxqiu-laptop =
-    { config, ... }:
+    { config, lib, ... }:
     {
-      my.sops.enable = true;
-      sops = {
+      sops = lib.mkIf config.my.sops.enable {
         defaultSopsFile = ../secrets/yuxqiu.yaml;
         age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
         age.generateKey = true;
--- nix/profiles/options/sops.nix
+++ nix/profiles/options/sops.nix
@@ -1,23 +1,16 @@
 { lib, ... }:
-let
-  sop_option = lib.mkOption {
-    type = lib.types.submodule {
-      options = {
-        enable = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = "Whether to enable sops";
+{
+  flake.modules.generic.base = {
+    options.my.sops = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            description = "Whether to enable sops";
+          };
         };
       };
     };
-  };
-in
-{
-  flake.modules.homeManager.base = {
-    options.my.sops = sop_option;
-  };
-
-  flake.modules.systemManager.base = {
-    options.my.sops = sop_option;
   };
 }
