35ced50ead6e477e89361fbfbbd46995df53ef43
yuxqiu <yuxqiu@proton.me>
2026-03-08 14:00:07 +0800

    doc: hm: systemd.packages option

--- nix/modules/services/systemd.nix
+++ nix/modules/services/systemd.nix
@@ -2,7 +2,11 @@
   flake.modules.homeManager.linux-base =
     { config, ... }:
     {
-      # allow systemd to discover user service files installed by nix
+      # A workaround to allow systemd to discover user service files
+      # installed by home-manager. Ideally, home-manager modules
+      # should properly use `systemd.packages` to specify which
+      # systemd services files should be copied into $XDG_DATA_HOME.
+      #
       # - https://unix.stackexchange.com/a/696035
       systemd.user.settings.Manager = {
         ManagerEnvironment = {
