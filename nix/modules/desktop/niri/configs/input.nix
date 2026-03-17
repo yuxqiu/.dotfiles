b7435d2277fa1b5e9f6ef736c6ce3c1ec552432e
yuxqiu <yuxqiu@proton.me>
2026-03-17 12:27:23 +0800

    feat(niri): configure compose key
    Set the right Alt key as the compose key for keyboard input.

--- nix/modules/desktop/niri/configs/input.nix
+++ nix/modules/desktop/niri/configs/input.nix
@@ -1,17 +1,23 @@
 {
   flake.modules.homeManager.linux-desktop = {
     wayland.windowManager.niri.settings.input = {
-      workspace-auto-back-and-forth = [];
+      workspace-auto-back-and-forth = [ ];
 
       touchpad = {
-        tap = [];
-        dwt = [];
-        natural-scroll = [];
-        middle-emulation = [];
+        tap = [ ];
+        dwt = [ ];
+        natural-scroll = [ ];
+        middle-emulation = [ ];
         accel-speed = 0.2;
       };
 
       mouse.accel-speed = 0.2;
+
+      keyboard = {
+        xkb = {
+          options = "compose:ralt";
+        };
+      };
 
       mod-key = "Alt";
       mod-key-nested = "Super";
