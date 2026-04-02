039e3afb06b8d02e12b9d5e5defa1943dd630ddf
yuxqiu <yuxqiu@proton.me>
2026-04-02 11:32:42 +0800

    feat(xdg): add fusermount3 wrapper for xdg portal

--- nix/modules/desktop/xdg.nix
+++ nix/modules/desktop/xdg.nix
@@ -11,4 +11,20 @@
     xdg.portal.enable = true;
     xdg.terminal-exec.enable = true;
   };
+
+  flake.modules.systemManager.desktop =
+    { pkgs, ... }:
+    {
+      # fusermount3 is required by xdg-document-portal
+      environment.systemPackages = with pkgs; [ fuse3 ];
+
+      security.wrappers = {
+        fusermount3 = {
+          setuid = true;
+          owner = "root";
+          group = "root";
+          source = "${pkgs.fuse3}/bin/fusermount3";
+        };
+      };
+    };
 }
