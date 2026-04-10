d5cd5d6aa107be71662b67538c351d958de24919
yuxqiu <yuxqiu@proton.me>
2026-04-10 10:23:18 +0800

    feat(activitywatch): add integrations and categories

---
+++ nix/modules/services/activitywatch/activitywatch.nix
@@ -0,0 +1,63 @@
+let
+  aw-listening-address = {
+    address = "127.0.0.1";
+    port = 5600;
+  };
+in
+{
+  flake.modules.homeManager.linux-desktop =
+    { pkgs, ... }:
+    {
+      services.activitywatch = {
+        enable = true;
+        settings = { } // aw-listening-address;
+        watchers = {
+          aw-watcher-afk = {
+            package = pkgs.activitywatch;
+            settings = {
+              timeout = 300;
+              poll_time = 2;
+            };
+          };
+
+          aw-watcher-window-wayland = {
+            package = pkgs.aw-watcher-window-wayland;
+            settings = {
+              poll_time = 1;
+              exclude_title = true;
+            };
+          };
+        };
+      };
+
+      programs.zed-editor = {
+        extensions = [ "activitywatch" ];
+        userSettings.lsp = {
+          activitywatch = {
+            settings = aw-listening-address;
+          };
+        };
+      };
+
+      programs.firefox.policies = {
+        ExtensionSettings = {
+          "{ef87d84c-2127-493f-b952-5b4e744245bc}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/{ef87d84c-2127-493f-b952-5b4e744245bc}/latest.xpi";
+            installation_mode = "normal_installed";
+          };
+        };
+        "3rdparty".Extensions = {
+          "{ef87d84c-2127-493f-b952-5b4e744245bc}" = {
+            "consentOfflineDataCollection" = true;
+          };
+        };
+      };
+    };
+
+  flake.modules.systemManager.desktop = {
+    services.tailscale.serveHttpsTargets = {
+      "${aw-listening-address.port}" =
+        "http://${aw-listening-address.address}:${aw-listening-address.port}";
+    };
+  };
+}
