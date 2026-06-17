643eb611b26704fa7cadfeaadaae366ceab7b75e
yuxqiu <yuxqiu@proton.me>
2026-06-17 16:14:11 +0800

    feat(steam): configure with proton-ge and system-composer

--- nix/modules/gaming/steam.nix
+++ nix/modules/gaming/steam.nix
@@ -1,7 +1,13 @@
 {
-  flake.modules.nixos.steam = {
+  flake.modules.nixos.steam = { pkgs, ... }: {
     programs.steam = {
       enable = true;
+      package = pkgs.steam.override {
+        extraArgs = "-system-composer";
+      };
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
       remotePlay.openFirewall = false;
       dedicatedServer.openFirewall = false;
       localNetworkGameTransfers.openFirewall = false;
