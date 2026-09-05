8029a84efa62b9808f6e1bfe6fd5c2486eb36fe3
yuxqiu <yuxqiu@proton.me>
2026-09-05 15:04:41 -0400

    refactor: update journald config and firefox findbar css
    - Convert journald extraConfig to structured settings
    - Rewrite Firefox findbar CSS for a cleaner flex layout
    - Update flake.lock dependencies

--- nix/modules/services/journald.nix
+++ nix/modules/services/journald.nix
@@ -1,16 +1,16 @@
 {
   flake.modules.nixos.journald = {
-    services.journald.extraConfig = ''
-      SystemMaxUse=50M
-      SystemMaxFileSize=50M
-      MaxRetentionSec=30day
-      Compress=yes
-      ForwardToSyslog=no
-      ForwardToKMsg=no
-      ForwardToConsole=no
-      ForwardToWall=no
-      RateLimitIntervalSec=30s
-      RateLimitBurst=10000
-    '';
+    services.journald.settings.Journal = {
+      SystemMaxUse = "50M";
+      SystemMaxFileSize = "50M";
+      MaxRetentionSec = "30day";
+      Compress = "yes";
+      ForwardToSyslog = "no";
+      ForwardToKMsg = "no";
+      ForwardToConsole = "no";
+      ForwardToWall = "no";
+      RateLimitIntervalSec = "30s";
+      RateLimitBurst = "10000";
+    };
   };
 }
